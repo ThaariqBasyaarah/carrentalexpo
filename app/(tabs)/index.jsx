423fde60cb7a71cf09e79c87b22ba945a1db541f
M Amiruddin Irsyad <amiruddinid@gmail.com>
2024-08-22 21:56:42 +0700

    Slicing & Integrasi Home & Detail

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,10 +1,45 @@
 import { Image, StyleSheet, View, Text, Button } from "react-native";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
+import ParallaxFlatList from "@/components/ParallaxFlatList";
 import Constants from "expo-constants";
+import { Col, Row } from "../../components/Grid";
+import ButtonIcon from "../../components/ButtonIcon";
+import CarList from "../../components/CarList";
+import { useState, useEffect } from "react";
 
 export default function HomeScreen() {
+  const [cars, setCars] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController(); // UseEffect cleanup untuk menghindari memory Leak
+    const signal = controller.signal;  // UseEffect cleanup
+
+    setLoading(true); //loading state
+    const getData = async () => {
+      try{
+        const response = await fetch(
+          "https://api-car-rental.binaracademy.org/customer/car",
+          { signal: signal }  // UseEffect cleanup
+        );
+        const body = await response.json();
+        setCars(body);
+      } catch(e) { // Error Handling
+        if (err.name === 'AbortError') {
+          console.log('successfully aborted');
+        } else {
+          console.log(err)
+        }
+      }
+    };
+    getData();
+    return () => {
+        // cancel request sebelum component di close
+        controller.abort();
+    };
+  }, []);
+
   return (
-    <ParallaxScrollView
+    <ParallaxFlatList
       headerBackgroundColor={{ light: "#A43333", dark: "#A43333" }}
       headerImage={
         <View style={styles.container}>
@@ -20,29 +55,63 @@ export default function HomeScreen() {
           </View>
         </View>
       }
-    >
-      <View style={styles.banner}>
-        <View style={styles.bannerContainer}>
-          <View style={styles.bannerTextContainer}>
-            <Text style={styles.bannerText}>Sewa Mobil Berkualitas di kawasanmu</Text>
-            <Button 
-              color='#3D7B3F'
-              title="Sewa Mobil"/>
+      banner={
+        <>
+          <View style={styles.banner}>
+            <View style={styles.bannerContainer}>
+              <View style={styles.bannerTextContainer}>
+                <Text style={styles.bannerText}>
+                  Sewa Mobil Berkualitas di kawasanmu
+                </Text>
+                <Button color="#3D7B3F" title="Sewa Mobil" />
+              </View>
+              <View>
+                <Image source={require("@/assets/images/img_car.png")} />
+              </View>
+            </View>
           </View>
           <View>
-            <Image
-              source={require("@/assets/images/img_car.png")}
-            />
+            <Row justifyContent={"space-between"}>
+              <Col>
+                <ButtonIcon text={'Sewa Mobil'} name={"car-outline"} color={"#ffffff"} />
+              </Col>
+              <Col>
+                <ButtonIcon text={'Oleh-Oleh'} name={"cube-outline"} color={"#ffffff"} />
+              </Col>
+              <Col>
+                <ButtonIcon text={'Penginapan'} name={"key-outline"} color={"#ffffff"} />
+              </Col>
+              <Col>
+                <ButtonIcon text={'Wisata'} name={"camera-outline"} color={"#ffffff"} />
+              </Col>
+            </Row>
           </View>
-        </View>
-      </View>
-    </ParallaxScrollView>
+        </>
+      }
+      loading={loading}
+      data={cars}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <CarList
+          style={{marginHorizontal:20}}
+          key={item.id}
+          image={{ uri: item.image }}
+          carName={item.name}
+          passengers={5}
+          baggage={4}
+          price={item.price}
+        />
+      )}
+      viewabilityConfig={{
+        waitForInteraction: true,
+      }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: Constants.statusBarHeight,
+    paddingTop: Constants.statusBarHeight + 10,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
@@ -58,24 +127,22 @@ const styles = StyleSheet.create({
   },
   banner: {
     backgroundColor: "#AF392F",
-    marginTop: -140,
-    overflow:'hidden',
-    paddingTop: 20,
-    borderRadius:5
+    marginTop: -100,
+    overflow: "hidden",
+    borderRadius: 5,
   },
   bannerContainer: {
     flexDirection: "row",
     alignItems: "flex-end",
     justifyContent: "space-between",
   },
-  bannerTextContainer:{
-    width: '45%',
-    padding:10,
-    paddingBottom: 25
+  bannerTextContainer: {
+    width: "45%",
+    padding: 15,
   },
   bannerText: {
     color: "#ffffff",
     fontFamily: "Poppins",
-    fontSize:16
+    fontSize: 16,
   },
 });
